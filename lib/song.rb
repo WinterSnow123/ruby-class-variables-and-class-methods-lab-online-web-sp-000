9958864aceb5861f3b67dff89ec02b79b2365b9d
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-26 20:29:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each do{|genre| genre_count.include?(genre) ? genre_count[genre] += 1 : genre_count[genre] = 1}
+    @@genres.each {|genre| genre_count.include?(genre) ? genre_count[genre] += 1 : genre_count[genre] = 1}
   end
 
 end
