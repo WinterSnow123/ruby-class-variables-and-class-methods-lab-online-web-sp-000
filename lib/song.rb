42752e4dd640067b0a54e88ae4860bac7d83f7b3
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-26 20:35:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,4 +34,9 @@ class Song
     genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each {|artist| artist_count.include?(artist) ? artist_count[artist] += 1 : artist_count[artist] = 1}
+    artist_count
+  end
 end
